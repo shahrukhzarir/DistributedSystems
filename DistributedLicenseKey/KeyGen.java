b6735c981c2cb86b2bd7af325d2da983061729f0
Shahrukh <shahrukh.zarir@uoit.net>
2021-04-16 14:30:10 -0400

    Delete DistributedLicenseKey directory

--- DistributedLicenseKey/KeyGen.java
+++
@@ -1,8 +0,0 @@
-
-public interface KeyGen extends java.rmi.Remote {
-   public final static String SERVICENAME="KeyGenerator";
-   public String generateKey(String name) throws java.rmi.RemoteException;
-   public boolean login(String username, String key) throws java.rmi.RemoteException;
-   public String register(String username, String key) throws java.rmi.RemoteException;
-   public int calcFac(int n) throws java.rmi.RemoteException;
-}
